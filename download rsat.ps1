6334f8389da164a3d93f801f620402c4e007287f
LordHagen <olehag04@nfk.no>
2017-08-03 13:47:07 +0200

    Updated script

--- download rsat.ps1
+++ download rsat.ps1
@@ -2,14 +2,27 @@
 
 import-module bitstransfer
 
-$RSATx64_url = https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/WindowsTH-RSAT_WS2016-x64.msu
-$RSATx86_url = https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/WindowsTH-RSAT_WS2016-x86.msu
-$fildir = ""
+#Remote Server Administration Tools, download location.
+$RSATx64_url = 'https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/WindowsTH-RSAT_WS2016-x64.msu'
+$RSATx86_url = 'https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/WindowsTH-RSAT_WS2016-x86.msu'
 
+if([environment]::Is64BitOperatingSystem -eq $True )
+{
+    $RSAT_url = $RSATx64_url
+}
+else
+{
+    $RSAT_url = $RSATx86_url
+}
+
+#File location.
+$filedir = ""
+
+#Downloads file and puts it in $filedir.
 {
     $start_time = Get-Date
 
-    Start-BitsTransfer -Source $RSAT_url -Destination $fildir
+    Start-BitsTransfer -Source $RSAT_url -Destination $filedir
 
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
