0b6e4227f726fd2bb1c92443f61f5c9d7d9afd7d
Ole Martin Hagen <olehag04@nfk.no>
2017-08-10 10:20:51 +0200

    Small changes to script

--- GET-RSAT.ps1
+++ GET-RSAT.ps1
@@ -1,4 +1,26 @@
 #Lord Hagen / olehag04@nfk.no
+#Windows 10 RSAT Downloader!
+
+
+#File location.
+$filedir = "c:\installfiles"
+
+if((Test-Path $filedir) -eq 0)
+{ 
+    mkdir $filedir
+    Clear-Host
+
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host "Created folder '$filedir'" -ForegroundColor Green
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+
+    Start-Sleep -s 2.5
+    Clear-Host
+}
 
 import-module bitstransfer
 
@@ -9,21 +31,28 @@ $RSATx86_url = 'https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A
 if([environment]::Is64BitOperatingSystem -eq $True )
 {
     $RSAT_url = $RSATx64_url
+    Write-Host "Downloading 64Bit Version!" -ForegroundColor Green
 }
 else
 {
     $RSAT_url = $RSATx86_url
+    Write-Host "Downloading 32Bit Version!" -ForegroundColor Green
 }
 
-#File location.
-$filedir = ''
 
 #Downloads file and puts it in $filedir.
 
 $start_time = Get-Date
 
 Start-BitsTransfer -Source $RSAT_url -Destination $filedir
-
+Clear-Host
+Write-Host
+Write-Host
+Write-Host "File downloaded to: $filedir, " -NoNewline
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+Write-Host
 
-Start-Sleep -s 2
+#Press a button to exit
+Write-Host ""
+Write-Host "Press any button to exit..." -ForegroundColor Yellow
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
